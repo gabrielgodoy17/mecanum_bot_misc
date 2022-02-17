d67c5f6679fe2486cebc58e40f9e27eb28d31997
gabrielgodoy17 <godoygabriel07@gmail.com>
2022-02-17 18:44:59 -0300

    Remove timer and rework logic

--- mecanum_bot_misc/teleop.py
+++ mecanum_bot_misc/teleop.py
@@ -9,90 +9,27 @@ class KeyboardRead(Node):
     def __init__(self):
         super().__init__('keyboard_read')
         self.publisher_ = self.create_publisher(String, 'directions', 10)
-        timer_period = 0.005  # seconds
-
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
-
-    def timer_callback(self):
-        global to_send_old
-        global to_send_new
-
-        #if to_send_old != to_send_new :
-        msg = String()
-        msg.data = to_send_new
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
-        to_send_old=to_send_new
 
 
-def on_activate_FWD():
-    global to_send_new
-    to_send_new = "FWD"
-
-def on_activate_BKWD():
-    global to_send_new
-    to_send_new = "BKWD" 
-
-def on_activate_L():
-    global to_send_new
-    to_send_new = "L"
-
-def on_activate_R():
-    global to_send_new
-    to_send_new = "R"   
-
-def on_activate_FWD_L():
-    global to_send_new
-    to_send_new = "FWD_L"
-
-def on_activate_FWD_R():
-    global to_send_new
-    to_send_new = "FWD_R"
-
-def on_activate_BKWD_L():
-    global to_send_new
-    to_send_new = "BKWD_L"
-
-def on_activate_BKWD_R():
-    global to_send_new
-    to_send_new = "BKWD_R"
-
-def on_activate_CW():
-    global to_send_new
-    to_send_new = "CW"
-
-def on_activate_CCW():
-    global to_send_new
-    to_send_new = "CCW"
-
-def on_activate_STOP():
-    global to_send_new
-    to_send_new = "STOP"
-
-def on_activate_EXIT():
-    global to_send_new
-    to_send_new = "EXIT"
-
-
-def on_press(key):    
-    func = dict.get(key, on_activate_STOP)
-    func()
+def on_press(key, node):    
+    msg = String()
+    msg.data = dict.get(key, 'STOP')
+    node.publisher_.publish(msg)
+    
 
 dict = {
-    keyboard.KeyCode(char = '8') : on_activate_FWD,
-    keyboard.KeyCode(char = '2') : on_activate_BKWD,
-    keyboard.KeyCode(char = '4') : on_activate_L,
-    keyboard.KeyCode(char = '6') : on_activate_R,
-    keyboard.KeyCode(char = '7') : on_activate_FWD_L,
-    keyboard.KeyCode(char = '9') : on_activate_FWD_R,
-    keyboard.KeyCode(char = '1') : on_activate_BKWD_L,
-    keyboard.KeyCode(char = '3') : on_activate_BKWD_R,
-    keyboard.KeyCode(char = 'x') : on_activate_CW,
-    keyboard.KeyCode(char = 'z') : on_activate_CCW,
-    keyboard.Key.space : on_activate_STOP,
-    keyboard.Key.esc : on_activate_EXIT  
+    keyboard.KeyCode(char = '8') : 'FWD',
+    keyboard.KeyCode(char = '2') : 'BKWD',
+    keyboard.KeyCode(char = '4') : 'L',
+    keyboard.KeyCode(char = '6') : 'R',
+    keyboard.KeyCode(char = '7') : 'FWD_L',
+    keyboard.KeyCode(char = '9') : 'FWD_R',
+    keyboard.KeyCode(char = '1') : 'BKWD_L',
+    keyboard.KeyCode(char = '3') : 'BKWD_R',
+    keyboard.KeyCode(char = 'x') : 'CW',
+    keyboard.KeyCode(char = 'z') : 'CCW',
+    keyboard.Key.space : 'STOP',
+    keyboard.Key.esc : 'EXIT'  
 }
 
 
@@ -101,40 +38,17 @@ def main(args=None):
     rclpy.init(args=args)
     keyboard_read = KeyboardRead()
 
-    global to_send_old
-    global to_send_new
-    to_send_old = " "
-    to_send_new = "STOP"
+    msg = String()
+    msg.data = 'STOP'
+    keyboard_read.publisher_.publish(msg)
 
     listener = keyboard.Listener(
-        on_press=on_press) 
+        on_press=lambda event: on_press(event, keyboard_read)) 
     listener.start()
 
+    keyboard_read.get_logger().info('Teleop ready')
     rclpy.spin(keyboard_read)
-    # with keyboard.GlobalHotKeys({
-    # '8' : on_activate_FWD,
-    # '2' : on_activate_BKWD,
-    # '4' : on_activate_L,
-    # '6' : on_activate_R,
-    # '7' : on_activate_FWD_L,
-    # '9' : on_activate_FWD_R,
-    # '1' : on_activate_BKWD_L,
-    # '3' : on_activate_BKWD_R,
-    # 'x' : on_activate_CW,
-    # 'z' : on_activate_CCW,
-    # '<space>' : on_activate_STOP,
-    # }) as h: h.join()
 
-    
-    # algo.press('A')
-    # algo.release('A')
-    
-    # with algo.pressed('8') : on_activate_FWD
-
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     keyboard_read.destroy_node()
     rclpy.shutdown()
 
